6c8ed5425cd7f5d25ce275507504c0f66da96deb
Vinícius Souza <tradutorvinicius@gmail.com>
2023-09-18 19:22:01 -0300

    pipeline ETL com Python

---
+++ etl.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import requests
+import json
+import openai
+
+openai.api_key = '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'
+sdw_api_url = 'https://sdw-2023-prd.up.railway.app'
+
+# extract
+df = pd.read_csv('userIds.csv')
+user_ids = df['UserID'].tolist()
+
+
+def get_user(id):
+  response = requests.get(f'{sdw_api_url}/users/{id}')
+
+  if response.status_code == 200:
+    return response.json() 
+  else:
+    None
+
+users = [user for id in user_ids if (user := get_user(id)) is not None]
+
+#transform
+def generate_ai_news(user):
+  completion = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+      {
+          "role": "system",
+          "content": "Você é um especialista sobre o universo Marvel e seus super-heróis."
+      },
+      {
+          "role": "user",
+          "content": f"Crie uma mensagem para {user['name']} sobre os poderes e habilidades de algum super-herói (máximo de 200 caracteres)"
+      }
+    ]
+  )
+  return completion.choices[0].message.content.strip('\"')
+
+for user in users:
+  news = generate_ai_news(user)
+  print(news)
+  user['news'].append({
+      "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
+      "description": news
+  })
+
+# load
+def update_user(user):
+  response = requests.put(f"{sdw_api_url}/users/{user['id']}", json=user)
+  return True if response.status_code == 200 else False
+
+for user in users:
+  success = update_user(user)
+  print(f"User {user['name']} updated? {success}!")
